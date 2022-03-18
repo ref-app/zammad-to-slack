0e6ea15182d9bf5707e14483bef1e54a0d98b239
Per Bergland <per@refapp.se>
2022-03-18 16:19:20 +0100

    get call

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
 #!/usr/bin/env ts-node-script
-import * as express from "express"
+import * as express from "express";
 
-const app = express()
+const app = express();
 
 const slackToken = process.env["SLACK_TOKEN"];
 
 app.use(express.json());
 
-app.post("/zammad",(req,res)=>{
-    console.log(req.body);
-    res.status(200);
-    res.end();
-})
+app.get("/", (_req, res) => {
+  res.write("Nothing to see here, move on");
+  res.status(200);
+  res.end();
+});
+
+app.post("/zammad", (req, res) => {
+  console.log(req.body);
+  res.status(200);
+  res.end();
+});
 
 app.listen(3001);
