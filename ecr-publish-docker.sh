e03099c6fd0760073b44104d0d8106b1b4968ea0
Per Bergland <per@refapp.se>
2022-03-28 17:47:10 +0200

    ecr-public

--- ecr-publish-docker.sh
+++ ecr-publish-docker.sh
@@ -2,16 +2,10 @@
 # depends on awsudo from https://github.com/makethunder/awsudo
 set -eo pipefail
 
-function getRegion() {
-    echo $AWS_DEFAULT_REGION
-}
-
+# These are our settings, customise to your own needs by passing in command line arguments
 AWS_ECR_PROFILE=${1:-refapp-devresources-admin}
+AWS_ECR_REGISTRY_ALIAS=${2:-refapp}
 
-AWS_ECR_ACCOUNT_ID=$(awsudo -u ${AWS_ECR_PROFILE} aws sts get-caller-identity --query "Account" --output text)
-AWS_DEFAULT_REGION=$(awsudo -u ${AWS_ECR_PROFILE} bash -c 'echo `echo $AWS_DEFAULT_REGION`')
-export ECR_URL="${AWS_ECR_ACCOUNT_ID}.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com"
-echo $ECR_URL
-awsudo -u ${AWS_ECR_PROFILE} bash -c 'aws ecr get-login-password|docker login --username AWS --password-stdin "$ECR_URL" && ./build-docker.sh "$ECR_URL"'
-# 
-# 
+export ECR_PUBLIC_REPO=public.ecr.aws/${AWS_ECR_REGISTRY_ALIAS}
+echo "Logging in and pushing to $ECR_PUBLIC_REPO"
+awsudo -u ${AWS_ECR_PROFILE} bash -c 'aws ecr-public get-login-password --region us-east-1|docker login --username AWS --password-stdin public.ecr.aws && ./build-docker.sh "$ECR_PUBLIC_REPO"'
